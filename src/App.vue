0a139a71c8059ecdf9f62bbf879e9c387e68597d
ElliotThiebaut <elliotthiebaut1@gmail.com>
2022-06-25 01:04:02 +0200

    :fire: Cleaned template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hi me" />
-  <div class="my-10">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  components: {HelloWorld}
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,8 @@
   <h1 class="text-2xl text-purple-500">This is the home page</h1>
 </template>
 
-<script>
-export default {
+<script setup>
 
-}
 </script>
 
 <style scoped>
